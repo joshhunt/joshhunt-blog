b6732909a5952ac6dfbc7287cccedf8c13cc5254
joshhunt <josh@trtr.co>
2024-03-01 23:47:04 +0000

    less assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/mode-toggle";
+import { SpeedInsights as VercelSpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -49,6 +50,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           </div>
 
           <VercelAnalytics />
+          <VercelSpeedInsights />
         </ThemeProvider>
       </body>
     </html>
